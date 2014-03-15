813e69df29e42db2fbea9f12e2e07eac546900d8
Bert Wijnants <bert.wijnants@gmail.com>
2014-03-15 17:29:30 +0100

    server side flashing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-
+var socket;
 var output;
+var intervalId;
 
 function init() {
     output = $("#color");
+    socket =  io.connect("http://"+window.location.hostname);
+    
+    blink("#FF0000", 100, 600);
 
-    requestInterval(function() { 
-        blink("#FF0000", 300); 
-    }, 1000);
+    socket.on('data', onDataEvent);
 }
 
-function blink(color, duration) {
+function onDataEvent(data) {
+    console.log(data);
+    blink(data.color, data.duration, data.interval);
+    //socket.emit('my other event', { my: 'data' });
+}
+
+function blink(color, duration, interval) {
+    clearRequestInterval(intervalId);
+    intervalId = requestInterval(function() { 
+        flash(color, duration); 
+    }, interval);
+}
+
+function flash(color, duration) {
     console.log("blink", color, duration);
     setColor(color);
     requestTimeout(function() {
